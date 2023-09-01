ed076cb2700b1249611b95fbe4ba0f4e7b494252
xycukrzn <xyc_uk@icloud.com>
2023-09-01 20:52:57 +0300

    programStart

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Что-то очень важное
+Console.WriteLine("Введите элементы массива через пробел:");
+    string input = Console.ReadLine();
+    string[] inputArray = input.Split(' ');
+
+    string[] resultArray = FilterStrings(inputArray);
+
+    Console.WriteLine("Результат:");
+    foreach (string str in resultArray)
+    {
+        Console.WriteLine(str);
+    }
+    
+
+static string[] FilterStrings(string[] inputArray)
+{
+    int count = 0;
+    foreach (string str in inputArray)
+    {
+        if (str.Length <= 3)
+        {
+            count++;
+        }
+    }
+
+    string[] resultArray = new string[count];
+    int index = 0;
+    foreach (string str in inputArray)
+    {
+        if (str.Length <= 3)
+        {
+            resultArray[index++] = str;
+        }
+    }
+
+    return resultArray;
+}
